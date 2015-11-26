655a617e6ebe4d0c8a594a7d9c805e55541829a4
Martin Choraine <mchoraine@MacBook-Pro-de-Martin.local>
2015-11-26 19:27:09 +0100

    Gros trou de balle

--- web/app/controllers/home.js
+++ web/app/controllers/home.js
@@ -2,6 +2,7 @@ var express = require('express'),
   router = express.Router(),
   mongoose = require('mongoose'),
   passport = require('passport'),
+  AdminModel = mongoose.model('admins'),
   isUserLogIn = require('../services/utils');
 
 module.exports = function (app) {
@@ -10,10 +11,14 @@ module.exports = function (app) {
 
 // Page d'accueil
 router.get('/', isUserLogIn, function (req, res, next) {
-    res.render('index', {
-      title: 'Liste des utilisateurs',
-      user : req.user,
-    })
+  AdminModel.find( function(err, usersList) {
+      res.render('index', {
+        title: 'Liste des utilisateurs',
+        user : req.user,
+        users : usersList
+      })
+    }
+  )
 });
 
 // Page de connexion
