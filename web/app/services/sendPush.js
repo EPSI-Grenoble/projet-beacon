a61b05074c9366d143ca7d9824b8d7587296e670
Martin Choraine <martin.choraine@gmail.com>
2015-12-05 10:49:59 +0100

    Comment bitch

--- web/app/services/sendPush.js
+++ web/app/services/sendPush.js
@@ -4,11 +4,24 @@ var gcm = require('node-gcm'),
   mongoose = require('mongoose'),
   MessageModel = mongoose.model('messages');
 
+/**
+ * Une classe pour envoyer des notifications push grâce à Google Cloud Messaging (GCM)
+ * @type {SendPush}
+ */
 module.exports = class SendPush {
+  /**
+   * On initialise la classe avec l'ID du message
+   * @param messageID
+     */
   constructor(messageID) {
     this.messageID = messageID;
   }
 
+  /**
+   * Methode pour envoyer le message immédiatement
+   * On récupère le message à partir de son ID puis on envoi la notif à tous les destinataires du messages
+   * TODO pour l'insant on récupère pas les device_token du user, il faut donc récupèrer pour chaque user son device_token puis envoyer une notif
+   */
   sendNow(){
     var device_tokens = [];
     var GCM = new gcm.Message();
@@ -29,6 +42,11 @@ module.exports = class SendPush {
     });
   }
 
+  /**
+   * On veut envoyer un notif dans le futur
+   * TODO il faudra donc mettre en place une cron et envoyer les notifs à ce moment là
+   * @param dateToSend
+     */
   sendOn(dateToSend){
 
   }
